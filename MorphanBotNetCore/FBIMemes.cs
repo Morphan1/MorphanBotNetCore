f869846bc6e311fe3a39bc1204a60fa9db563b0e
Morphan1 <morphanone@gmail.com>
2020-05-29 02:36:12 -0400

    fbi open up

---
+++ MorphanBotNetCore/FBIMemes.cs
@@ -0,0 +1,21 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorphanBotNetCore
+{
+    public class FBIMemes : ModuleBase<SocketCommandContext>
+    {
+        private const string FBIMemeFolder = "data/fbimemes/";
+
+        [Command("fbi")]
+        public async Task CallTheFBI()
+        {
+            string[] files = Directory.GetFiles(FBIMemeFolder);
+            await Context.Channel.SendFileAsync(files[Utilities.random.Next(files.Length)]);
+        }
+    }
+}
